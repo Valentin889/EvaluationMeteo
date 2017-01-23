0657610e17b0b904082b1302840cdba72a800cc8
Valentin889 <valentin.schneeberger@rpn.ch>
2017-01-23 13:26:35 +0100

    ajout d'un bouton valider au formulaire, à son clique il affiche la météo pour la localité choisie

--- src/index.php
+++ src/index.php
@@ -31,58 +31,60 @@
                     <h3>
                             affichage météo
                     </h3>
-                    <select class="form-control" name ="ville" size="1">
-                        
-                        <?php
-                            $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
-                            $response =  $client->get('list-cities');
-                            $data = json_decode(($response),true);
-                            
-                            for($i=0; $i<count($data);$i++)
-                            {
-                                echo '<option>'.$data[$i]["name"].'</option>';
-                            }
-                            
-                            
-                            
-                        ?>
+                    <form>
+                        <select class="form-control" name ="ville" size="1">
+                            <?php
+                                $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
+                                $response =  $client->get('list-cities');
+                                $data = json_decode(($response),true);
 
-                    </select>
+                                for($i=0; $i<count($data);$i++)
+                                {
+                                    echo '<option>'.$data[$i]["name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                        <button type="submit" class="btn btn-primary">valider</button>
 			<table class="table">
-				<thead>
-					<tr>
-						<th>
-							jour
-						</th>
-						<th>
-							photo
-						</th>
-                                                <th>
-                                                    température minimum
-                                                </th>
-                                                <th>
-                                                    température maximum
-                                                </th>
-					</tr>
-				</thead>
-				<tbody>
-                                     <?php
-                                        $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
-                                        $response =  $client->get('Neuchatel');
-                                        $data = json_decode(($response),true);
-                                        $nbJours = 5;
-                                        for($i =0; $i < $nbJours; $i++)
-                                        {
-                                            echo '<tr>';
-                                            echo '<td>'.$data["fcst_day_$i"]['day_short'].'</td>';
-                                            echo '<td> <img src = '.$data["fcst_day_$i"]['icon'].' alt ="previson"></td>';
-                                            echo '<td>'.$data["fcst_day_$i"]['tmin'].'</td>';
-                                            echo '<td>'.$data["fcst_day_$i"]['tmax'].'</td>';
-                                            echo '</tr>';
-                                        }
-                                    ?>
-				</tbody>
+                            <thead>
+                                    <tr>
+                                            <th>
+                                                    jour
+                                            </th>
+                                            <th>
+                                                    photo
+                                            </th>
+                                            <th>
+                                                température minimum
+                                            </th>
+                                            <th>
+                                                température maximum
+                                            </th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                                 <?php
+                                    $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
+                                    $response =  $client->get('Neuchatel');
+                                    if(!empty($_GET['ville']))
+                                    {
+                                        $response = $client->get($_GET['ville']);
+                                    }
+                                    $data = json_decode(($response),true);
+                                    $nbJours = 5;
+                                    for($i =0; $i < $nbJours; $i++)
+                                    {
+                                        echo '<tr>';
+                                        echo '<td>'.$data["fcst_day_$i"]['day_short'].'</td>';
+                                        echo '<td> <img src = '.$data["fcst_day_$i"]['icon'].' alt ="previson"></td>';
+                                        echo '<td>'.$data["fcst_day_$i"]['tmin'].'</td>';
+                                        echo '<td>'.$data["fcst_day_$i"]['tmax'].'</td>';
+                                        echo '</tr>';
+                                    }
+                                ?>
+                            </tbody>
 			</table>
+                    </form>
 		</div> 
 	</div>
 </div>
