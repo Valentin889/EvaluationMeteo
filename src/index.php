3e68cb35d51f94b4315e3129f4bf280ecd146c93
Valentin889 <valentin.schneeberger@rpn.ch>
2017-01-24 10:27:53 +0100

    gestion de la redondence des données, on ne peut pas rentrer une ville déjà présente

--- src/index.php
+++ src/index.php
@@ -1,129 +1,132 @@
 <?php
-    require_once 'ApiSimpleGetRestClient.php';
-    $client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
-    if(isset($_GET['EnvoieVille']))
-    {
-        $response =  $client->get($_GET['Nouvelle_Ville']);
-        $data = json_decode(($response),true);
-        if(isset($data['errors']))
-        {
-            echo 'erreur';
+require_once 'ApiSimpleGetRestClient.php';
+$client = new ApiSimpleGetRestClient('http://www.prevision-meteo.ch/services/json');
+$array = array(
+                                0 => 'Neuchâtel',
+                                1 => 'La-chaux-de-fonds',
+                                2 => 'Berne',
+                                3 => 'Lausanne'
+                                    );
+        $array+=$_COOKIE;
+
+
+
+if (isset($_GET['EnvoieVille'])) {
+    $response = $client->get($_GET['Nouvelle_Ville']);
+    $data = json_decode(($response), true);
+    if (isset($data['errors'])) {
+        echo 'erreur';
+    } else {
+        $b = true;
+        foreach ($array as $value) {
+            if ($_GET['Nouvelle_Ville'] == $value) {
+                $b = false;
+            }
         }
-        else
-        {
-            setcookie($_GET['Nouvelle_Ville'],$_GET['Nouvelle_Ville'],time() + (86400 * 30),"/"); //86400 = 1 day
+        if ($b) {
+            setcookie($_GET['Nouvelle_Ville'], $_GET['Nouvelle_Ville'], time() + (86400 * 30), "/"); //86400 = 1 day
         }
     }
-    
+}
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap 3, from LayoutIt!</title>
+        <title>Bootstrap 3, from LayoutIt!</title>
 
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="author" content="LayoutIt!">
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="author" content="LayoutIt!">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
 
-  </head>
-  <body>
+    </head>
+    <body>
 
-     
-      
-      
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
+
+
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
                     <h3>
-                            affichage météo
+                        affichage météo
                     </h3>
                     <form>
                         <label> localité </label>
                         <select class="form-control" name ="ville" size="1">
-                            <option>Neuchâtel</option>
-                            <option>La-chaux-de-fonds</option>
-                            <option>Berne</option>
-                            <option>Lausanne</option>
+
                             <?php
-                            foreach ($_COOKIE as $value)
-                            {
-                                echo '<option>'.$value.'</otpion>';
-                            }
+                                foreach ($array as $value)
+                                {
+                                    echo '<option>'.$value.'</option>';
+                                }
                             ?>
                         </select>
                         <label> nombre de jours à afficher </label>
                         <select class="form-control" name="jours" size ="1">
                             <?php
-                                for($i=1; $i<6;$i++)
-                                {
-                                     echo '<option>'.$i.'</option>'; 
-                                }
+                            for ($i = 1; $i < 6; $i++) {
+                                echo '<option>' . $i . '</option>';
+                            }
                             ?>
                         </select>
                         <button type="submit" class="btn btn-primary" name ="validation"> valider</button>
-			<table class="table">
+                        <table class="table">
                             <thead>
-                                    <tr>
-                                            <th>
-                                                    jour
-                                            </th>
-                                            <th>
-                                                    photo
-                                            </th>
-                                            <th>
-                                                température minimum
-                                            </th>
-                                            <th>
-                                                température maximum
-                                            </th>
-                                    </tr>
+                                <tr>
+                                    <th>
+                                        jour
+                                    </th>
+                                    <th>
+                                        photo
+                                    </th>
+                                    <th>
+                                        température minimum
+                                    </th>
+                                    <th>
+                                        température maximum
+                                    </th>
+                                </tr>
                             </thead>
                             <tbody>
-                                 <?php
-                                    
-                                    $response =  $client->get('Neuchatel');
-                                    $nbJours = 5;
-                                    if(isset($_GET['validation']))
-                                    {
-                                        $response = $client->get($_GET['ville']);
-                                        $nbJours=$_GET['jours'];
-                                    }
-                                    $data = json_decode(($response),true);
-                                    
-                                    for($i =0; $i < $nbJours; $i++)
-                                    {
-                                        echo '<tr>';
-                                        echo '<td>'.$data["fcst_day_$i"]['day_short'].'</td>';
-                                        echo '<td> <img src = '.$data["fcst_day_$i"]['icon'].' alt ="previson"></td>';
-                                        echo '<td>'.$data["fcst_day_$i"]['tmin'].'</td>';
-                                        echo '<td>'.$data["fcst_day_$i"]['tmax'].'</td>';
-                                        echo '</tr>';
-                                    }
+                                <?php
+                                $response = $client->get('Neuchatel');
+                                $nbJours = 5;
+                                if (isset($_GET['validation'])) {
+                                    $response = $client->get($_GET['ville']);
+                                    $nbJours = $_GET['jours'];
+                                }
+                                $data = json_decode(($response), true);
+
+                                for ($i = 0; $i < $nbJours; $i++) {
+                                    echo '<tr>';
+                                    echo '<td>' . $data["fcst_day_$i"]['day_short'] . '</td>';
+                                    echo '<td> <img src = ' . $data["fcst_day_$i"]['icon'] . ' alt ="previson"></td>';
+                                    echo '<td>' . $data["fcst_day_$i"]['tmin'] . '</td>';
+                                    echo '<td>' . $data["fcst_day_$i"]['tmax'] . '</td>';
+                                    echo '</tr>';
+                                }
                                 ?>
                             </tbody>
-			</table>
-                    </form>
-                    <form>
+                        </table>
                         <label> nom de la nouvelle ville </label>
                         <input type="text" name="Nouvelle_Ville">
                         <button type="submit" class="btn btn-primary" name ="EnvoieVille"> valider</button>
                     </form>
-		</div> 
-	</div>
-</div>
+                </div> 
+            </div>
+        </div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-  </body>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
-                                   
